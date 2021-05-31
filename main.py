2336e53c2fc90ea5728f8870c3a96b68e1d03104
Gunou Park <gunou88@gmail.com>
2021-06-01 00:24:33 +0900

    Update kotlin environment

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ if __name__ == '__main__':
 
     class_name = java_file.split('/')[-1].split('.')[0]
     classpath = 'evosuite-tests:lib/evosuite-standalone-runtime-1.1.0.jar:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar'
+    kotlin_libs = [
+        'lib/kotlin-stdlib.jar',
+        'lib/kotlin-stdlib-jdk8.jar',
+        'lib/kotlin-reflect.jar',
+        'lib/kotlin-test.jar',
+    ]
+
+    classpath += ':' + ':'.join(kotlin_libs)
 
     subprocess.run(['javac', java_file, '-d', 'java_classpath'])
-    subprocess.run(['kotlinc', kotlin_file, '-d', 'kotlin_classpath'])
+    subprocess.run(['kotlinc', kotlin_file, '-jvm-target', '1.8', '-d', 'kotlin_classpath'])
     subprocess.run(['java', '-jar', 'lib/evosuite-1.1.0.jar', '-class', class_name, '-projectCP', 'java_classpath'])
     subprocess.run(['javac', 'evosuite-tests/' + class_name + '_ESTest.java', '-classpath', 'java_classpath:' + classpath])
     subprocess.run(['java', '-cp', 'java_classpath:' + classpath, 'org.junit.runner.JUnitCore', class_name + '_ESTest'])
