fb6c9ddbf7e626201e5be3ddbf5760aec09339ac
Gunou Park <gunou88@gmail.com>
2021-06-02 00:25:16 +0900

    Update java classpaths for external libraries

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ if __name__ == '__main__':
     kotlin_file = args.kotlin
 
     class_name = java_file.split('/')[-1].split('.')[0]
-    classpath = 'evosuite-tests:lib/evosuite-standalone-runtime-1.1.0.jar:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar'
+
+    default_libs = [
+        'lib/evosuite-standalone-runtime-1.1.0.jar',
+        'lib/junit-4.12.jar',
+        'lib/hamcrest-core-1.3.jar',
+    ]
+    case_specific_libs = [
+        'lib/commons-net-3.8.0.jar',
+    ]
     kotlin_libs = [
         'lib/kotlin-stdlib.jar',
         'lib/kotlin-stdlib-jdk8.jar',
@@ -20,11 +28,16 @@ if __name__ == '__main__':
         'lib/kotlin-test.jar',
     ]
 
+    classpath = 'evosuite-tests'
+    classpath += ':' + ':'.join(default_libs)
+    classpath += ':' + ':'.join(case_specific_libs)
     classpath += ':' + ':'.join(kotlin_libs)
 
-    subprocess.run(['javac', java_file, '-d', 'java_classpath'])
-    subprocess.run(['kotlinc', kotlin_file, '-jvm-target', '1.8', '-d', 'kotlin_classpath'])
-    subprocess.run(['java', '-jar', 'lib/evosuite-1.1.0.jar', '-class', class_name, '-projectCP', 'java_classpath'])
+    print(' '.join(['java', '-jar', 'lib/evosuite-1.1.0.jar', '-class', class_name, '-projectCP', 'java_classpath']))
+
+    subprocess.run(['javac', java_file, '-d', 'java_classpath', '-cp', ':'.join(case_specific_libs)])
+    subprocess.run(['kotlinc', kotlin_file, '-jvm-target', '1.8', '-d', 'kotlin_classpath', '-cp', ':'.join(case_specific_libs)])
+    subprocess.run(['java', '-jar', 'lib/evosuite-1.1.0.jar', '-class', class_name, '-projectCP', 'java_classpath' + ':' + ':'.join(case_specific_libs)])
     subprocess.run(['javac', 'evosuite-tests/' + class_name + '_ESTest.java', '-classpath', 'java_classpath:' + classpath])
     subprocess.run(['java', '-cp', 'java_classpath:' + classpath, 'org.junit.runner.JUnitCore', class_name + '_ESTest'])
     subprocess.run(['java', '-cp', 'kotlin_classpath:' + classpath, 'org.junit.runner.JUnitCore', class_name + '_ESTest'])
